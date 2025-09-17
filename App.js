9a34250e2095acc0209cfd5afbcacda538f3d47c
Nnlllllll <nathannovais020@gmail.com>
2025-09-17 09:18:37 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { TelaInicial } from './src/Telas/TelaInicial';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="Inicio">
+        <stack.Screen name="Inicio" component={TelaInicial} />
+      </stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
